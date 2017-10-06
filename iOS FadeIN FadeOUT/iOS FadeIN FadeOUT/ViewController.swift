b1929a09896ec1fdea5772f7fa32205cc9a8d8db
san2ride <sanchezjason@me.com>
2017-10-06 00:04:47 -0600

    FadeOut/FadeIn Switch

---
+++ iOS FadeIN FadeOUT/iOS FadeIN FadeOUT/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  iOS FadeIN FadeOUT
+//
+//  Created by don't touch me on 10/5/17.
+//  Copyright © 2017 trvl, LLC. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let asset = NSDataAsset(name: "thestonefoxes-everybodyknows") else {
+            print("Error Loading Audio.");
+            return
+        }
+        do {
+            player = try AVAudioPlayer(data: asset.data)
+        } catch {
+            print("Error Playing Audio.");
+            return
+        }
+        player.volume = 0
+        player.numberOfLoops = -1
+        player.play()
+    }
+
+    @IBAction func fadeSwitchChanged(_ sender: UISwitch) {
+        if sender.isOn {
+            // Fading in
+            player.setVolume(1, fadeDuration: 2)
+        } else {
+            
+            // Fading Out
+            player.setVolume(0, fadeDuration: 2)
+        }
+    }
+    
+
+}
+
